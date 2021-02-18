231a7d89500a0e5e908260c59b25176d86b27749
TomSuzuki <tomsuzuki2633@gmail.com>
2021-02-18 21:23:00 +0900

    #10 HTMLの生成。

--- assets/js/recipe_page.js
+++ assets/js/recipe_page.js
@@ -1,13 +1,13 @@
-window.addEventListener("DOMContentLoaded", () => {
-    if (document.getElementById("frame_recipe") !== undefined) {
-        let id = getParams()["id"];
-        if (id !== undefined) accessServer(`/recipe/data?id=${id}`, (result) => {
-            setRecipiPage(JSON.parse(result));
-        });
-    }
-});
+// window.addEventListener("DOMContentLoaded", () => {
+//     if (document.getElementById("frame_recipe") !== undefined) {
+//         let id = getParams()["id"];
+//         if (id !== undefined) accessServer(`/recipe/data?id=${id}`, (result) => {
+//             setRecipiPage(JSON.parse(result));
+//         });
+//     }
+// });
 
-// setRecipiPage ...レシピページを表示する。
-function setRecipiPage(json) {
-    console.log(json);
-}
+// // setRecipiPage ...レシピページを表示する。
+// function setRecipiPage(json) {
+//     console.log(json);
+// }
